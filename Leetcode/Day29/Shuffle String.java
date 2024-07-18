fe771e0a6765c1a5ce84b7809e9fe5636ecb0294
Pranesh-S15 <praneshselvaraj.in@gmail.com>
2024-07-18 21:53:23 +0530

    Day29 Uploaded

---
+++ Leetcode/Day29/Shuffle String.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] ans = new char[s.length()];
+
+        for (int i = 0; i < indices.length; ++i) {
+            ans[indices[i]] = s.charAt(i);
+        }
+
+        return new String(ans);
+    }
+}
